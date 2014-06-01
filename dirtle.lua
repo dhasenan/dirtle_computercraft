834cf5c4554d709630bcac220eabc5710458a62b
Chris Wright <dhasenan@gmail.com>
2014-06-01 00:47:58 +0000

    and tests!

--- dirtle.lua
+++ dirtle.lua
@@ -32,8 +32,13 @@
 -- A coordinate is a 3-tuple representing a point in space.
 -- It does not have any specified coordinate system.
 Coord = {}
-local _coordMt = {__index = Coord}
-function Coord:new(x, y, z)
+local _coordMt = {
+  __index = Coord,
+  __tostring = function(self)
+    return string.format('(%d, %d, %d)', self.x, self.y, self.z)
+  end
+}
+function Coord.new(x, y, z)
   local o = {}
   setmetatable(o, _coordMt)
   o.x = x
@@ -43,15 +48,23 @@ function Coord:new(x, y, z)
 end
 
 function Coord:add(other)
-  return Coord:new(self.x + other.x, self.y + other.y, self.z + other.z)
+  return Coord.new(self.x + other.x, self.y + other.y, self.z + other.z)
+end
+
+function Coord:plus(other)
+  return self:add(other)
 end
 
 function Coord:sub(other)
-  return Coord:new(self.x - other.x, self.y - other.y, self.z - other.z)
+  return Coord.new(self.x - other.x, self.y - other.y, self.z - other.z)
+end
+
+function Coord:minus(other)
+  return self:sub(other)
 end
 
 function Coord:mul(s)
-  return Coord:new(self.x * s, self.y * s, self.z * s)
+  return Coord.new(self.x * s, self.y * s, self.z * s)
 end
 
 function Coord:equals(other)
@@ -62,41 +75,41 @@ function Coord:round()
   function roundInt(x)
     return math.floor(x + 0.5)
   end
-  return Coord:new(roundInt(self.x), roundInt(self.y), roundInt(self.z))
+  return Coord.new(roundInt(self.x), roundInt(self.y), roundInt(self.z))
 end
 
 -- Add two coordinates, generating a new coordinate representing their sum.
 
 -- Direction constants.
-NORTH = Coord:new( 1,  0,  0)
-EAST  = Coord:new( 0,  1,  0)
-SOUTH = Coord:new(-1,  0,  0)
-WEST  = Coord:new( 0, -1,  0)
-UP    = Coord:new( 0,  0,  1)
-DOWN  = Coord:new( 0,  0, -1)
+NORTH = Coord.new( 1,  0,  0)
+EAST  = Coord.new( 0,  1,  0)
+SOUTH = Coord.new(-1,  0,  0)
+WEST  = Coord.new( 0, -1,  0)
+UP    = Coord.new( 0,  0,  1)
+DOWN  = Coord.new( 0,  0, -1)
 
 Shape = {}
 local _shapeMt = {__index = Shape}
-function Shape:new()
+function Shape.new()
   local o = {}
   setmetatable(o, _shapeMt)
   return o
 end
 
 function Shape:intersect(other)
-  return IntersectShape:new({self, other})
+  return IntersectShape.new({self, other})
 end
 
 function Shape:union(other)
-  return UnionShape:new({self, other})
+  return UnionShape.new({self, other})
 end
 
 function Shape:minus(other)
-  return DifferenceShape:new(self, other)
+  return DifferenceShape.new(self, other)
 end
 
 function Shape:xor(other)
-  return XorShape:new(self, other)
+  return XorShape.new(self, other)
 end
 
 function Shape:contains(coord)
@@ -114,7 +127,7 @@ IntersectShape = {}
 setmetatable(IntersectShape, {__index = Shape})
 local _intersectShapeMt = {__index = IntersectShape}
 
-function IntersectShape:new(children)
+function IntersectShape.new(children)
   local o = {}
   setmetatable(o, _intersectShapeMt)
   o.children = children
@@ -155,7 +168,7 @@ UnionShape = {}
 setmetatable(UnionShape, {__index = Shape})
 local _unionShapeMt = {__index = UnionShape}
 
-function UnionShape:new(children)
+function UnionShape.new(children)
   local o = {}
   setmetatable(o, _unionShapeMt)
   o.children = children
@@ -203,7 +216,7 @@ DifferenceShape = {}
 setmetatable(DifferenceShape, {__index = Shape})
 local _differenceShapeMt = {__index = DifferenceShape}
 
-function DifferenceShape:new(minuend, subtrahend)
+function DifferenceShape.new(minuend, subtrahend)
   local o = {}
   setmetatable(o, _differenceShapeMt)
   o.minuend = minuend
@@ -235,7 +248,7 @@ RectangleShape = {}
 setmetatable(RectangleShape, {__index = Shape})
 local _rectangleShapeMt = {__index = RectangleShape}
 
-function RectangleShape:new(start, finish)
+function RectangleShape.new(start, finish)
   start, finish = normalizeCoordRange(start, finish)
   local o = {}
   setmetatable(o, _rectangleShapeMt)
@@ -247,9 +260,9 @@ end
 function RectangleShape:contains(coord)
   return coord.x <= self.finish.x and
          coord.x >= self.start.x and
-         coord.y <= self.start.y and
+         coord.y <= self.finish.y and
          coord.y >= self.start.y and
-         coord.z <= self.start.z and
+         coord.z <= self.finish.z and
          coord.z >= self.start.z
 end
 
@@ -261,7 +274,7 @@ function RectangleShape:coords_iter()
     if z > self.finish.z then
       return nil
     end
-    local c = Coord:new(x, y, z)
+    local c = Coord.new(x, y, z)
     x = x + 1
     if x > self.finish.x then
       x = self.start.x
@@ -281,12 +294,13 @@ CylinderShape = {}
 setmetatable(CylinderShape, {__index = Shape})
 local _cylinderShapeMt = {__index = CylinderShape}
 
-function CylinderShape:new(origin, radius, height)
+function CylinderShape.new(origin, radius, height)
   local o = {}
   setmetatable(o, _cylinderShapeMt)
-  o.origin = center
+  o.origin = origin
   o.radius = radius
   o.height = height
+  assert(o.origin ~= nil, 'nil origin')
   return o
 end
 
@@ -295,9 +309,10 @@ function CylinderShape:contains(coord)
     return false
   end
   -- A circle's border is defined as x^2 + y^2 = r^2.
-  -- x^2 is always positive, so we can compare simply:
+  -- x^2 is always positive, so we can compare simply.
+  -- To make the circles look fuller and less pointy, we add 0.5 to the radius.
   local offset = coord:minus(self.origin)
-  return math.pow(offset.x, 2) + math.pow(offset.y, 2) <= math.pow(self.radius, 2)
+  return math.pow(offset.x, 2) + math.pow(offset.y, 2) <= math.pow(0.5 + self.radius, 2)
 end
 
 function CylinderShape:coords_iter()
@@ -305,20 +320,22 @@ function CylinderShape:coords_iter()
   -- Here, we are going in a square from -r to +r in each direction
   -- and filtering with contains().
   local level = 0
-  local x = -1 * self.radius
-  local y = -1 * self.radius
+  local high = 1 + self.radius
+  local low = -1 * high
+  local x = low
+  local y = low
   return function()
     while true do
       if level >= self.height then
         return nil
       end
-      local c = Coord:new(self.origin.x + x, self.origin.y + y, self.origin.z + level)
+      local c = Coord.new(self.origin.x + x, self.origin.y + y, self.origin.z + level)
       x = x + 1
-      if x > self.radius then
-        x = -1 * self.radius
+      if x > high then
+        x = low
         y = y + 1
-        if y > self.radius then
-          y = -1 * self.radius
+        if y > high then
+          y = low
           level = level + 1
         end
       end
@@ -330,7 +347,7 @@ function CylinderShape:coords_iter()
 end
 
 
-local _dirtle_pos = Coord:new(0, 0, 0)
+local _dirtle_pos = Coord.new(0, 0, 0)
 local _dirtle_facing = NORTH
 
 function turnLeft()
@@ -515,7 +532,7 @@ function placeBlock(coords)
 end
 
 function getPosition()
-  return Coord:new(_dirtle_pos.x, _dirtle_pos.y, _dirtle_pos.z);
+  return Coord.new(_dirtle_pos.x, _dirtle_pos.y, _dirtle_pos.z);
 end
 
 function nextItem()
@@ -532,8 +549,8 @@ function nextItem()
 end
 
 function normalizeCoordRange(start, finish)
-  local s2 = Coord:new(0, 0, 0)
-  local f2 = Coord:new(0, 0, 0)
+  local s2 = Coord.new(0, 0, 0)
+  local f2 = Coord.new(0, 0, 0)
   s2.x = math.min(start.x, finish.x)
   s2.y = math.min(start.y, finish.y)
   s2.z = math.min(start.z, finish.z)
@@ -582,34 +599,34 @@ function buildHollowBlock(start, finish, afterPlace)
   -- Build the lower platform. Leave the margin alone -- one primary usecase of afterPlace is to
   -- add torches, and that doesn't work so well otherwise.
   buildSolidBlock(
-    Coord:new(start.x + 1, start.y + 1, start.z),
-    Coord:new(finish.x - 1, finish.y - 1, start.z),
+    Coord.new(start.x + 1, start.y + 1, start.z),
+    Coord.new(finish.x - 1, finish.y - 1, start.z),
     afterPlace)
   -- Same for the top
   buildSolidBlock(
-    Coord:new(start.x + 1, start.y + 1, finish.z),
-    Coord:new(finish.x - 1, finish.y - 1, finish.z),
+    Coord.new(start.x + 1, start.y + 1, finish.z),
+    Coord.new(finish.x - 1, finish.y - 1, finish.z),
     afterPlace)
 
   -- Now build the left and right walls
   buildSolidBlock(
-    Coord:new(start.x, start.y, start.z),
-    Coord:new(start.x, finish.y, finish.z),
+    Coord.new(start.x, start.y, start.z),
+    Coord.new(start.x, finish.y, finish.z),
     afterPlace)
   buildSolidBlock(
-    Coord:new(finish.x, start.y, start.z),
-    Coord:new(finish.x, finish.y, finish.z),
+    Coord.new(finish.x, start.y, start.z),
+    Coord.new(finish.x, finish.y, finish.z),
     afterPlace)
 
   -- Now the front and back. Here, the corners have already been filled.
   -- So we have to be careful not to fill those spaces.
   buildSolidBlock(
-    Coord:new(start.x + 1, start.y, start.z),
-    Coord:new(finish.x - 1, start.y, finish.z),
+    Coord.new(start.x + 1, start.y, start.z),
+    Coord.new(finish.x - 1, start.y, finish.z),
     afterPlace)
   buildSolidBlock(
-    Coord:new(start.x + 1, finish.y, start.z),
-    Coord:new(finish.x - 1, finish.y, finish.z),
+    Coord.new(start.x + 1, finish.y, start.z),
+    Coord.new(finish.x - 1, finish.y, finish.z),
     afterPlace)
 end
 
---
+++ dirtle_test.lua
@@ -0,0 +1,185 @@
+require('dirtle')
+-- os.loadAPI() loads stuff into a new namespace.
+-- require() loads stuff into the global namespace.
+-- This is a hack to make the API work as we want.
+local dirtle = _G
+
+
+function test(name, fn)
+  io.write('test ' .. name .. '...')
+  fn()
+  io.write('passed\n')
+end
+
+test('coord equality', function()
+  assert(dirtle.Coord.new(0, 0, 0):equals(dirtle.Coord.new(0, 0, 0)))
+  assert(not dirtle.Coord.new(0, 0, 0):equals(dirtle.Coord.new(0, 1, 0)))
+end)
+
+test('coord addition', function()
+  local c1 = dirtle.Coord.new(3, 8, -3)
+  local c2 = dirtle.Coord.new(4, -1, -1)
+  local sum = c1:add(c2)
+  assert(sum:equals(dirtle.Coord.new(7, 7, -4)))
+end)
+
+test('coord subtraction', function()
+  local c1 = dirtle.Coord.new(3, 8, -3)
+  local c2 = dirtle.Coord.new(4, -1, -1)
+  local diff = c1:sub(c2)
+  assert(diff:equals(dirtle.Coord.new(-1, 9, -2)))
+end)
+
+test('coord multiplication', function()
+  local c1 = dirtle.Coord.new(3, 8, -3)
+  local product = c1:mul(7)
+  assert(product:equals(dirtle.Coord.new(21, 56, -21)))
+end)
+
+test('coord round', function()
+  local c1 = dirtle.Coord.new(3.7, 8.1, -3)
+  local rounded = c1:round()
+  assert(rounded:equals(dirtle.Coord.new(4, 8, -3)))
+end)
+
+test('rectangle coord iter', function()
+  local rect = dirtle.RectangleShape.new(
+    dirtle.Coord.new(1, 2, 1),
+    dirtle.Coord.new(3, 3, 2))
+  local coords = {}
+  for coord in rect:coords_iter() do
+    table.insert(coords, coord)
+  end
+  local expected = {
+    dirtle.Coord.new(1, 2, 1),
+    dirtle.Coord.new(2, 2, 1),
+    dirtle.Coord.new(3, 2, 1),
+    dirtle.Coord.new(1, 3, 1),
+    dirtle.Coord.new(2, 3, 1),
+    dirtle.Coord.new(3, 3, 1),
+
+    dirtle.Coord.new(1, 2, 2),
+    dirtle.Coord.new(2, 2, 2),
+    dirtle.Coord.new(3, 2, 2),
+    dirtle.Coord.new(1, 3, 2),
+    dirtle.Coord.new(2, 3, 2),
+    dirtle.Coord.new(3, 3, 2),
+  }
+
+  for i, c1 in pairs(expected) do
+    local found = false
+    for j, c2 in pairs(coords) do
+      if c1:equals(c2) then
+        found = true
+        break
+      end
+    end
+    assert(found)
+  end
+end)
+
+test('rectangle contains all its coords', function()
+  local rect = dirtle.RectangleShape.new(
+    dirtle.Coord.new(1, 2, 1),
+    dirtle.Coord.new(3, 3, 2))
+  local expected = {
+    dirtle.Coord.new(1, 2, 1),
+    dirtle.Coord.new(2, 2, 1),
+    dirtle.Coord.new(3, 2, 1),
+    dirtle.Coord.new(1, 3, 1),
+    dirtle.Coord.new(2, 3, 1),
+    dirtle.Coord.new(3, 3, 1),
+
+    dirtle.Coord.new(1, 2, 2),
+    dirtle.Coord.new(2, 2, 2),
+    dirtle.Coord.new(3, 2, 2),
+    dirtle.Coord.new(1, 3, 2),
+    dirtle.Coord.new(2, 3, 2),
+    dirtle.Coord.new(3, 3, 2),
+  }
+
+  for i, c1 in pairs(expected) do
+    assert(rect:contains(c1))
+  end
+end)
+
+test('rectangle does not contain external coords', function()
+  local rect = dirtle.RectangleShape.new(
+    dirtle.Coord.new(1, 2, 1),
+    dirtle.Coord.new(3, 3, 2))
+  assert(not rect:contains(dirtle.Coord.new(3, 3, 3)))
+  assert(not rect:contains(dirtle.Coord.new(3, 4, 2)))
+  assert(not rect:contains(dirtle.Coord.new(1, 1, 1)))
+end)
+
+test('cylinder-4 contains', function()
+  local cylinder = dirtle.CylinderShape.new(dirtle.Coord.new(0, 0, 0), 4, 1)
+  local s = ''
+  for x = -5, 5 do
+    for y = -5, 5 do
+      if cylinder:contains(dirtle.Coord.new(x, y, 0)) then
+        s = s .. 'x'
+      else
+        s = s .. ' '
+      end
+    end
+    s = s .. '\n'
+  end
+  local expected = [[   xxxxx   
+  xxxxxxx  
+ xxxxxxxxx 
+ xxxxxxxxx 
+ xxxxxxxxx 
+ xxxxxxxxx 
+ xxxxxxxxx 
+  xxxxxxx  
+   xxxxx   
+]]
+  function trim(str)
+    return str:gsub('^%s*(.-)%s*$', '%1')
+  end
+  assert(trim(s) == trim(expected))
+end)
+
+test('cylinder-4 coords', function()
+  local cylinder = dirtle.CylinderShape.new(dirtle.Coord.new(0, 0, 0), 4, 1)
+  local expected = 0
+  for x = -5, 5 do
+    for y = -5, 5 do
+      if cylinder:contains(dirtle.Coord.new(x, y, 0)) then
+        expected = expected + 1
+      end
+    end
+  end
+  local actual = 0
+  for c in cylinder:coords_iter() do
+    actual = actual + 1
+    assert(cylinder:contains(c))
+  end
+  assert(actual == expected)
+end)
+
+test('intersect two rectangles', function()
+  local rect1 = dirtle.RectangleShape.new(
+    dirtle.Coord.new(0, 0, 0),
+    dirtle.Coord.new(2, 2, 2))
+  local rect2 = dirtle.RectangleShape.new(
+    dirtle.Coord.new(-1, 1, 0),
+    dirtle.Coord.new(2, 2, 3))
+  local inter = rect1:intersect(rect2)
+  local expected = dirtle.RectangleShape.new(
+    dirtle.Coord.new(0, 1, 0),
+    dirtle.Coord.new(2, 2, 2))
+
+  local expectedCount = 0
+  local actualCount = 0
+  for c in expected:coords_iter() do
+    expectedCount = expectedCount + 1
+    assert(inter:contains(c))
+  end
+  for c in inter:coords_iter() do
+    actualCount = actualCount + 1
+    assert(expected:contains(c))
+  end
+  assert(actualCount == expectedCount)
+end)
